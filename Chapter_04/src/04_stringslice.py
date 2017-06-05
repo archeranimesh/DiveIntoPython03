b46f4232db6bbbd322566ea6e89203fd03386823
archeranimesh <animeshb@archerimagine.com>
2017-06-05 14:40:16 +0530

    string slice

---
+++ Chapter_04/src/04_stringslice.py
@@ -0,0 +1,7 @@
+a_string = "My alphabet starts where your alphabet ends."
+print("a_string:: ", a_string)
+print("a_string[3:11]: ", a_string[3:11])
+print("a_string[3:-3]: ", a_string[3:-3])
+print("a_string[0:2]: ", a_string[0:2])
+print("a_string[:18]: ", a_string[:18])
+print("a_string[18:]: ", a_string[18:])
