928a2af937c6f168a3f31874246a0ef40b15d16c
archeranimesh <animeshb@archerimagine.com>
2017-06-05 13:49:56 +0530

    Chapter 04 example

---
+++ Chapter_04/src/01_string.py
@@ -0,0 +1,4 @@
+s = '深入 Python'
+print("len(s): ", len(s))
+print("s[0]: ", s[0])
+print("s + 3", (s + '3'))
---
+++ Chapter_04/src/02_stringsFormat.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, '/home/animeshkbhadra/myWork/PythonLearning/books/DiveIntoPython03/Chapter_01/src/')
+import humansize
+
+username = "animeshb"
+passoword = "12345"
+print("{0}'s password is {1}".format(username, passoword))
+
+si_suffixes = humansize.SUFFIXES[1000]
+print("si_suffixes:: ", si_suffixes)
+print('1000{0[0]} = 1{0[1]}'.format(si_suffixes))
+print('1MB = 1000{0.modules[humansize].SUFFIXES[1000][0]}'.format(sys))
